2cf39eb7f3bf6b3ed8c23a07666cfd97959a01f3
inklbot <31788091+inklbot@users.noreply.github.com>
2023-09-20 04:24:08 +0900

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-GPU_INDEX=0  
+# GPU 사용량을 모니터링할 GPU 인덱스 설정
+GPU_INDEX=0  # 원하는 GPU 인덱스로 변경하세요
 
+# 원하는 GPU 사용량 임계값 설정 (10% 미만)
 THRESHOLD=10
 
-GPU_USAGE=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits -i $GPU_INDEX)
+while true; do
+  # GPU 사용량 확인
+  GPU_USAGE=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits -i $GPU_INDEX)
 
-if [ $GPU_USAGE -lt $THRESHOLD ]; then
-  cd quai-gpu-miner
+  # GPU 사용량이 임계값 미만인 경우 명령 실행
+  if [ $GPU_USAGE -lt $THRESHOLD ]; then
+    # quai-gpu-miner 디렉토리로 이동
+    cd quai-gpu-miner
 
-  cd build
+    # build 디렉토리로 이동
+    cd build
 
-  ./ethcoreminer/ethcoreminer -G -P stratum://65.109.11.250:3333
-else
-  echo "10% up"
-fi
+    # ethcoreminer 실행
+    ./ethcoreminer/ethcoreminer -G -P stratum://65.109.11.250:3333
+  else
+    echo "GPU 사용량이 10% 이상이므로 명령을 실행하지 않습니다."
+  fi
+
+  # 5초 동안 대기하고 다시 확인 (원하는 대기 시간으로 변경 가능)
+  sleep 5
+done
