1d1335eee59401423333271f5ea87867c6ee026a
inklbot <31788091+inklbot@users.noreply.github.com>
2023-09-21 07:36:51 +0900

    Update quai.sh

--- quai.sh
+++ quai.sh
@@ -2,10 +2,12 @@
 
 sudo apt update
 sudo apt install git make jq wget -y
+sleep 1
 
 echo -e "y\n" | sudo ufw enable
 sudo ufw allow 1:65535/tcp
 sudo ufw allow 1:65535/udp
+sleep 1
 
 git clone https://github.com/dominant-strategies/go-quai.git
 cd go-quai
@@ -13,10 +15,14 @@ NODE_LATEST_TAGS=$(curl -s https://api.github.com/repos/dominant-strategies/go-q
 git checkout $NODE_LATEST_TAGS
 cp network.env.dist network.env
 
+echo ""
+
 read -p "Please complete network.env configuration and press Enter."
 
 make go-quai
 make run
+
+sleep 3
 
 cd
 git clone https://github.com/dominant-strategies/go-quai-stratum
@@ -25,5 +31,7 @@ PROXY_LATEST_TAGS=$(curl -s https://github.com/dominant-strategies/go-quai-strat
 git checkout $PROXY_LATEST_TAGS
 cp config/config.example.json config/config.json
 make quai-stratum
+
+echo ""
 
 echo "./build/bin/quai-stratum --region=8583 --zone=8679"
