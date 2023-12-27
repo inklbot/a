dceedd571edf2053cb7e77d359f684d78c3b33ad
inklbot <31788091+inklbot@users.noreply.github.com>
2023-12-27 13:58:36 +0900

    Update op_node.sh

--- op_node.sh
+++ op_node.sh
@@ -18,30 +18,9 @@ export P2P_PRIV_KEY=f71a175a259ecc446d31e3ba3e2f623626cb83bfa6ed6ecc24dd94e99f8a
 export L1_RPC=https://bsc-dataseed.bnbchain.org
 export P2P_BOOTNODES="enr:-J24QA9sgVxbZ0KoJ7-1gx_szfc7Oexzz7xL2iHS7VMHGj2QQaLc_IQZmFthywENgJWXbApj7tw7BiouKDOZD4noWEWGAYppffmvgmlkgnY0gmlwhDbjSM6Hb3BzdGFja4PMAQCJc2VjcDI1NmsxoQKetGQX7sXd4u8hZr6uayTZgHRDvGm36YaryqZkgnidS4N0Y3CCIyuDdWRwgiMs"
 
-./op-node \
-  --l1.trustrpc \
-  --sequencer.l1-confs=15 \
-  --verifier.l1-confs=15 \
-  --l1.http-poll-interval 3s \
-  --l1.epoch-poll-interval 45s \
-  --l1.rpc-max-batch-size 20 \
+./bin/op-node \
+  --l1=ws://localhost:8546 --l2=ws//localhost:9001 \
   --rollup.config=./rollup.json \
-  --rpc.addr=0.0.0.0 \
-  --rpc.port=8546 \
-  --p2p.sync.req-resp \
-  --p2p.listen.ip=0.0.0.0 \
-  --p2p.listen.tcp=9003 \
-  --p2p.listen.udp=9003 \
-  --snapshotlog.file=./snapshot.log \
-  --p2p.bootnodes=$P2P_BOOTNODES \
-  --metrics.enabled \
-  --metrics.addr=0.0.0.0 \
-  --metrics.port=7300 \
-  --pprof.enabled \
-  --rpc.enable-admin \
-  --l1=${L1_RPC} \
-  --l2=${L2_RPC} \
+  --rpc.addr=127.0.0.1 \
   --l2.jwt-secret=./jwt.txt \
-  --l2.engine-sync=true \
-  --l2.skip-sync-start-check=true \  
-  --log.level=debug
+  --rpc.port=8546
