cc949d8c789f35dba73e090d2078ed499ff5c374
inklbot <31788091+inklbot@users.noreply.github.com>
2023-12-27 13:42:19 +0900

    Create op_node.sh

---
+++ op_node.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/bash
+
+set -ex
+
+cd op-node-data
+
+export L2_RPC=http://localhost:8551
+# replace the p2p private key with yours
+# you can generate a new one with `openssl rand -hex 32`
+export P2P_PRIV_KEY=ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+
+# for testnet
+# it's better to replace the L1_RPC with your own BSC Testnet RPC Endpoint for stability
+#export L1_RPC=https://bsc-testnet.bnbchain.org
+#export P2P_BOOTNODES="enr:-J24QGQBeMsXOaCCaLWtNFSfb2Gv50DjGOKToH2HUTAIn9yXImowlRoMDNuPNhSBZNQGCCE8eAl5O3dsONuuQp5Qix2GAYjB7KHSgmlkgnY0gmlwhDREiqaHb3BzdGFja4PrKwCJc2VjcDI1NmsxoQL4I9wpEVDcUb8bLWu6V8iPoN5w8E8q-GrS5WUCygYUQ4N0Y3CCIyuDdWRwgiMr,enr:-J24QJKXHEkIhy0tmIk2EscMZ2aRrivNsZf_YhgIU51g4ZKHWY0BxW6VedRJ1jxmneW9v7JjldPOPpLkaNSo6cXGFxqGAYpK96oCgmlkgnY0gmlwhANzx96Hb3BzdGFja4PrKwCJc2VjcDI1NmsxoQMOCzUFffz04eyDrmkbaSCrMEvLvn5O4RZaZ5k1GV4wa4N0Y3CCIyuDdWRwgiMr"
+
+# for mainnet
+export L1_RPC=https://bsc-dataseed.bnbchain.org
+export P2P_BOOTNODES="enr:-J24QA9sgVxbZ0KoJ7-1gx_szfc7Oexzz7xL2iHS7VMHGj2QQaLc_IQZmFthywENgJWXbApj7tw7BiouKDOZD4noWEWGAYppffmvgmlkgnY0gmlwhDbjSM6Hb3BzdGFja4PMAQCJc2VjcDI1NmsxoQKetGQX7sXd4u8hZr6uayTZgHRDvGm36YaryqZkgnidS4N0Y3CCIyuDdWRwgiMs"
+
+./op-node \
+  --l1.trustrpc \
+  --sequencer.l1-confs=15 \
+  --verifier.l1-confs=15 \
+  --l1.http-poll-interval 3s \
+  --l1.epoch-poll-interval 45s \
+  --l1.rpc-max-batch-size 20 \
+  --rollup.config=./rollup.json \
+  --rpc.addr=0.0.0.0 \
+  --rpc.port=8546 \
+  --p2p.sync.req-resp \
+  --p2p.listen.ip=0.0.0.0 \
+  --p2p.listen.tcp=9003 \
+  --p2p.listen.udp=9003 \
+  --snapshotlog.file=./snapshot.log \
+  --p2p.bootnodes=$P2P_BOOTNODES \
+  --metrics.enabled \
+  --metrics.addr=0.0.0.0 \
+  --metrics.port=7300 \
+  --pprof.enabled \
+  --rpc.enable-admin \
+  --l1=${L1_RPC} \
+  --l2=${L2_RPC} \
+  --l2.jwt-secret=./jwt.txt \
+  --l2.engine-sync=true \
+  --l2.skip-sync-start-check=true \  
+  --log.level=debug
